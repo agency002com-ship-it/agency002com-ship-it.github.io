93b1b2b60a327aaafde0a98035d11c5413c5089e
agency002com-ship-it <agency002.com@gmail.com>
2026-09-07 02:39:03 +0300

    Orange agency002.com and sebarv.com DNS for grok-cf €120 catalog patches.

--- ftp-drop/hub-hook.ps1
+++ ftp-drop/hub-hook.ps1
@@ -13,13 +13,16 @@ function Need-Flip {
   $cash = ''
   $pay = ''
   $eido = ''
+  $agency = ''
   try { $cash = (Invoke-WebRequest -Uri 'https://120.cash/' -UseBasicParsing).Content } catch { }
   try { $pay = (Invoke-WebRequest -Uri 'https://keychain.gr/pay.html' -UseBasicParsing).Content } catch { }
   try { $eido = (Invoke-WebRequest -Uri 'https://eidotevil.com/' -UseBasicParsing).Content } catch { }
+  try { $agency = (Invoke-WebRequest -Uri 'https://agency002.com/' -UseBasicParsing).Content } catch { }
   $needCash = $cash -match 'one working day'
   $needPay = $pay -match [regex]::Escape("a('https://120.cash/#brief', '120.cash');")
   $needEido = $eido -match [regex]::Escape('href="https://keychain.gr/pay.html?plan=cash_120"')
-  return ($needCash -or $needPay -or $needEido)
+  $needAgency = $agency -match [regex]::Escape('href="https://keychain.gr/pay.html?plan=cash_120"')
+  return ($needCash -or $needPay -or $needEido -or $needAgency)
 }
 
 if (-not (Need-Flip)) { exit 0 }
